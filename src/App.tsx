38a585b0715ccdc4455fba145eedeb8d4ad34ba8
Guilherme Santos <santosgui678@gmail.com>
2025-04-07 19:56:39 -0300

    Styling header, search and info

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,43 @@
-import './App.css'
+import styles from './App.module.css';
+import iconArrow from '../images/icon-arrow.svg';
 
 function App() {
 
   return (
     <>
-      <h1>IP address tracker</h1>
+    <header className={styles.header}>
+      <h2 className={styles.header__title}>IP Address Tracker</h2>
+
+      <div className={styles.search}>
+        <input type="number" className={styles.search__input} />
+        <button className={styles.search__button}>
+          <img src={iconArrow} alt="Search IP" />
+        </button>
+      </div>
+
+      <div className={styles.info}>
+        <div className={styles.info__card}>
+          <h6>Ip Address</h6>
+          <h3>192.212.174.101</h3>
+        </div>
+        
+        <div className={styles.info__card}>
+          <h6>Location</h6>
+          <h3>Brooklyn, NY 10001</h3>
+        </div>
+
+        <div className={styles.info__card}>
+          <h6>Timezone</h6>
+          <h3>UTC -05:00</h3>
+        </div>
+
+        <div className={styles.info__card}>
+          <h6>Isp</h6>
+          <h3>SpaceX Starlink</h3>
+        </div>
+      </div>
+
+    </header>
     </>
   )
 }
