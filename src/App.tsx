71c361d017a8127fd83f115b682d0bb840f9a430
Guilherme Santos <santosgui678@gmail.com>
2025-04-08 16:21:51 -0300

    Search ip address functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,48 @@
 import styles from './App.module.css';
 import iconArrow from '../images/icon-arrow.svg';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [ipAddressSearch, setIpAddressSearch] = useState<string>();
+
+  const [ipAddress, setIpAddress] = useState<string>('');
+  const [location , setLocation] = useState<string>('');
+  const [timezone, setTimezone] = useState<string>('');
+  const [isp, setIsp] = useState<string>('');
+
+  const searchIpAddress = async () => {
+    await axios.get(`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_IP_API_KEY}&ipAddress=${ipAddressSearch}`)
+      .then((response) => {
+        setIpAddress(response.data.ip);
+        setLocation(response.data.location.region + ', ' + response.data.location.country)
+        setTimezone(response.data.location.timezone)
+        setIsp(response.data.isp);
+
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
+
+  useEffect(() => {
+    const getIpAddress = async () => {
+      await axios.get(`https://geo.ipify.org/api/v2/country?apiKey=${import.meta.env.VITE_IP_API_KEY}`)
+        .then((response) => {
+          setIpAddress(response.data.ip);
+          setLocation(response.data.location.region + ',' + response.data.location.country)
+          setTimezone(response.data.location.timezone)
+          setIsp(response.data.isp);
+  
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+
+    getIpAddress();
+  }, []);
+
 
   return (
     <>
@@ -9,8 +50,8 @@ function App() {
       <h2 className={styles.header__title}>IP Address Tracker</h2>
 
       <div className={styles.search}>
-        <input type="number" className={styles.search__input} />
-        <button className={styles.search__button}>
+        <input type="string" className={styles.search__input} value={ipAddressSearch} onChange={(e: React.ChangeEvent<HTMLInputElement>) => setIpAddressSearch(e.target.value)}/>
+        <button className={styles.search__button} onClick={searchIpAddress}>
           <img src={iconArrow} alt="Search IP" />
         </button>
       </div>
@@ -18,28 +59,28 @@ function App() {
       <div className={styles.info}>
         <div className={styles.info__card}>
           <h6>Ip Address</h6>
-          <h3>192.212.174.101</h3>
+          <h3>{ipAddress}</h3>
         </div>
 
         <div className={styles.divider}></div>
         
         <div className={styles.info__card}>
           <h6>Location</h6>
-          <h3>Brooklyn, NY 10001</h3>
+          <h3>{location}</h3>
         </div>
 
         <div className={styles.divider}></div>
 
         <div className={styles.info__card}>
           <h6>Timezone</h6>
-          <h3>UTC -05:00</h3>
+          <h3>UTC {timezone}</h3>
         </div>
 
         <div className={styles.divider}></div>
 
         <div className={styles.info__card}>
           <h6>Isp</h6>
-          <h3>SpaceX Starlink</h3>
+          <h3>{isp}</h3>
         </div>
       </div>
 
